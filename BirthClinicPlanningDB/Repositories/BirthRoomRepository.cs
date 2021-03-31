61982b709206cd48f75c24e10bcea41cc8a6021d
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-31 22:16:33 +0200

    Added repository and methods for Birthrooms

---
+++ BirthClinicPlanningDB/Repositories/BirthRoomRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BirthClinicPlanningDB.Domain_objects;
+using Microsoft.EntityFrameworkCore;
+
+namespace BirthClinicPlanningDB.Repositories
+{
+    public class BirthRoomRepository:Repository<BirthRoom>, IBirthRoomRepository
+    {
+        public BirthRoomRepository(DbContext context) : base(context)
+        {
+        }
+
+        public List<BirthRoom> GetAllBirthsRooms()
+        {
+            return context.birthRooms.ToList();
+        }
+
+        public BirthRoom GetSingleBirthRoom(int id)
+        {
+            return context.birthRooms.SingleOrDefault(a => a.BirthRoomId == id);
+        }
+
+        public Context context
+        {
+            get { return Context as Context; }
+        }
+    }
+}
---
+++ BirthClinicPlanningDB/Repositories/IBirthRoomRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BirthClinicPlanningDB.Domain_objects;
+using BirthClinicPlanningDB.Models;
+
+namespace BirthClinicPlanningDB.Repositories
+{
+    public interface IBirthRoomRepository : IRepository<BirthRoom>
+    {
+        public List<BirthRoom> GetAllBirthsRooms();
+
+        public BirthRoom GetSingleBirthRoom(int id);
+    }
+}
