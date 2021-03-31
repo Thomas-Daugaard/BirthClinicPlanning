5025a3cb101743bb5fa1733a62f70cc5c624eb33
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-31 21:13:00 +0200

    Added entities to DbContext

--- BirthClinicPlanningDB/Context.cs
+++ BirthClinicPlanningDB/Context.cs
@@ -1,4 +1,5 @@
 ﻿using BirthClinicPlanningDB.Domain_objects;
+using BirthClinicPlanningDB.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BirthClinicPlanningDB
@@ -16,6 +17,14 @@ namespace BirthClinicPlanningDB
             base.OnModelCreating(modelBuilder);
         }
 
-        private DbSet<BirthClinic> birthClinics { get; set; }
+        public DbSet<Appointments> appointMents { get; set; }
+        public DbSet<Birth> births { get; set; }
+        public DbSet<BirthClinic> birthClinics { get; set; }
+        public DbSet<BirthRoom> birthRooms { get; set; }
+        public DbSet<Child> childs { get; set; }
+        public DbSet<Clinician> clinicians { get; set; }
+        public DbSet<FourHoursRestRoom> fourhourrestrooms { get; set; }
+        public DbSet<MaternityRoom> maternityrooms { get; set; }
+        public DbSet<Parents> parents { get; set; }
     }
 }
