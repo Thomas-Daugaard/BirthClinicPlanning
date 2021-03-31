1b6849c0c8d9065fee2799e77900c47cb60ac804
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-31 22:11:12 +0200

    added repository + methods for FourHourRestRoom

---
+++ BirthClinicPlanningDB/Repositories/FourHourRestRoomRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BirthClinicPlanningDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BirthClinicPlanningDB.Repositories
+{
+    public class FourHourRestRoomRepository: Repository<FourHoursRestRoom>, IFourHourRestRoomRepository
+    {
+        public FourHourRestRoomRepository(DbContext context) : base(context)
+        {
+        }
+
+        public List<FourHoursRestRoom> GetAllRestRoom()
+        {
+            return context.fourhourrestrooms.ToList();
+        }
+
+        public FourHoursRestRoom GetSingleRestRoom(int id)
+        {
+            return context.fourhourrestrooms.SingleOrDefault(a => a.FourHourRestRoomId == id);
+        }
+
+        public Context context
+        {
+            get { return Context as Context; }
+        }
+    }
+}
---
+++ BirthClinicPlanningDB/Repositories/IFourHourRestRoomRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BirthClinicPlanningDB.Domain_objects;
+using BirthClinicPlanningDB.Models;
+
+namespace BirthClinicPlanningDB.Repositories
+{
+    public interface IFourHourRestRoomRepository : IRepository<FourHoursRestRoom>
+    {
+        public List<FourHoursRestRoom> GetAllRestRoom();
+
+        public FourHoursRestRoom GetSingleRestRoom(int id);
+    }
+}
