416f0c9d0813e5508eff5ee7896734aa94d79bb9
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-04-06 13:24:14 +0200

    - Fixed display of clinicians

--- BirthClinicPlanningDB/DomainObjects/Clinician.cs
+++ BirthClinicPlanningDB/DomainObjects/Clinician.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,11 @@ namespace BirthClinicPlanningDB.DomainObjects
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        [NotMapped]
+        public string Display
+        {
+            get => $"{FirstName} {LastName}";
+        }
     }
 }
